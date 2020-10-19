1f6eb80824da8aadb89208958ac9a344d0c3f714
Karol Matusz <karol.matusz@uniteam.pl>
2020-10-19 11:36:50 +0200

    additional test cases

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -19,6 +19,16 @@ class LoginPage:
 
     __INVALID_CREDS_TEXT = (By.XPATH, "//div[contains(text(),'Invalid username/password.')]")
 
+    __FORGOT_PASSWORD_LINK = (By.XPATH, "//*[contains(text(),'Forgot password?')]")
+
+    __RESET_PASSWORD_TEXT = (By.XPATH, "//h1[contains(text(),'Reset your password')]")
+
+    __RESET_PASSWORD_EMAIL_FIELD = (By.NAME, "email")
+
+    __RESET_PASSWORD_BUTTON = (By.XPATH, "//button[contains(text(),'Reset password')]")
+
+    __RESET_PASSWORD_LINK_TEXT_AFTER_RESETTING_PASSWORD = (By.XPATH, "//div[contains(text(),'We sent reset password link to your e-mail.')]")
+
 
     def __init__(self, driver):
         self.__driver = driver
@@ -40,10 +50,34 @@ class LoginPage:
         self.__log.info("Clicking sign in button")
         driver.find_element(*self.__SIGN_IN_BUTTON).click()
 
-
     def is_invalid_creds_text_exists(self):
         driver = self.__driver
         return driver.find_element(*self.__INVALID_CREDS_TEXT).is_displayed()
+
+    def click_forgot_password_link(self):
+        driver = self.__driver
+        forgot_password_link_wait = Helper.wait_for_element_clickable(driver, self.__FORGOT_PASSWORD_LINK)
+        forgot_password_link_wait.click()
+
+    def is_reset_password_text_exists(self):
+        driver = self.__driver
+        return driver.find_element(*self.__RESET_PASSWORD_TEXT).is_displayed()
+
+    def fill_reset_password_email_field_and_click_reset_password_button(self, user_email):
+        driver = self.__driver
+        driver.find_element(*self.__RESET_PASSWORD_EMAIL_FIELD).send_keys(user_email)
+        driver.find_element(*self.__RESET_PASSWORD_BUTTON).click()
+
+    def is_we_sent_reset_password_link_exists(self):
+        driver = self.__driver
+        reset_password_text_wait = Helper.wait_for_element_clickable(driver, self.__RESET_PASSWORD_LINK_TEXT_AFTER_RESETTING_PASSWORD)
+        return driver.find_element(*self.__RESET_PASSWORD_LINK_TEXT_AFTER_RESETTING_PASSWORD).is_displayed()
+
+
+
+
+
+
 
 
 
--- tests/test_TC001_check_sign_in.py
+++ tests/test_TC001_check_sign_in.py
@@ -21,6 +21,7 @@ def driver():
 
     DriverFactory.close_driver()
 
+@pytest.mark.Login
 def test_check_sign_in_with_valid_credentials(driver):
     login_page = LoginPage(driver)
     login_page.load()
@@ -28,10 +29,28 @@ def test_check_sign_in_with_valid_credentials(driver):
     dashboards_page = DashboardPage(driver)
     assert dashboards_page.is_logged_successfully()
 
-
+@pytest.mark.Login
 def test_check_sign_in_with_invalid_credentials(driver):
     login_page = LoginPage(driver)
     login_page.load()
     login_page.fill_the_sign_in_form_and_login("invalid_email@example.com", "invalid_password")
     assert login_page.is_invalid_creds_text_exists()
+
+@pytest.mark.Password
+def test_forgot_password_link_check(driver):
+    login_page = LoginPage(driver)
+    login_page.load()
+    login_page.click_forgot_password_link()
+    assert login_page.is_reset_password_text_exists()
+
+@pytest.mark.Password
+def test_reset_password_check(driver):
+    login_page = LoginPage(driver)
+    login_page.load()
+    login_page.click_forgot_password_link()
+    assert login_page.is_reset_password_text_exists()
+    login_page.fill_reset_password_email_field_and_click_reset_password_button("john@example.com")
+    assert login_page.is_we_sent_reset_password_link_exists()
+
+
 
